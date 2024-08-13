81537e6568c87e2bd793c517c893a597e7b69423
Ryka Chandra <ryka@seas.upenn.edu>
2024-08-13 03:35:34 -0400

    Add files via upload

---
+++ hw4_test.py
@@ -0,0 +1,95 @@
+'''
+Ryka Chandra
+Period 3
+'''
+# this file is excluded from flake8
+# The tests you create will be graded
+
+from cse163_utils import assert_equals
+from document import Document
+from search_engine import SearchEngine
+
+
+def test_get_words(doc1, doc2, doc3):
+    """
+    Tests get_words method in Document class
+    """
+    assert_equals(sorted(['bestttttt', 'food', 'in', 'is', 'sush', 'the', 'world']), sorted(doc1.get_words()))
+    assert_equals(sorted(['clr', 'emerald', 'favorite', 'green', 'is', 'my']), sorted(doc2.get_words()))
+    assert_equals(sorted(['best', 'country', 'creek', 'go', 'high', 'i', 'in', 'north', 'school', 'the', 'to']), sorted(doc3.get_words()))
+
+def test_term_frequency(doc1, doc2, doc3):
+    """
+    Tests term_frequency method in Document class
+    """
+    assert_equals(0.2222222222222222, doc1.term_frequency('_food_'))
+    assert_equals(0.16666666666666666, doc2.term_frequency('favorite!!'))
+    assert_equals(0.08333333333333333, doc3.term_frequency('COuntrY'))
+
+def test_get_path(doc1, doc2, doc3):
+    """
+    Tests get_path method in Document class
+    """
+    assert_equals('ryka_test_corpus/best_food.txt', doc1.get_path())
+    assert_equals('ryka_test_corpus/favorite_color.txt', doc2.get_path())
+    assert_equals('ryka_test_corpus/high_school.txt', doc3.get_path())
+
+def test_calculate_idf(se):
+    """
+    Tests _calculate_idf method in SearchEngine class
+    """
+    assert_equals(0.4054651081081644, se._calculate_idf('in'))
+    assert_equals(0, se._calculate_idf('%%i!!n!!'))
+    assert_equals(0, se._calculate_idf('@#$ '))
+    assert_equals(1.0986122886681098, se._calculate_idf('emerald'))
+    
+    #tests blank parameter
+    assert_equals(0, se._calculate_idf(''))
+
+def test_single_item_query(se):
+    """
+    Tests search method in SearchEngine class for one word
+    and a series of odd characters with no space between them
+    """
+    assert_equals([], se.search('  hi'))
+    assert_equals(['ryka_test_corpus/favorite_color.txt'], se.search('EMerald'))
+    assert_equals(['ryka_test_corpus/high_school.txt'], se.search('&$nor*t^h'))
+    assert_equals(['ryka_test_corpus/best_food.txt'], se.search('sush1'))
+    assert_equals(['ryka_test_corpus/best_food.txt', 'ryka_test_corpus/high_school.txt'], se.search('in'))
+
+def test_multiple_item_query(se):
+    """
+    Tests search method in SearchEngine class for multiple
+    words and odd characters
+    """
+    assert_equals(['ryka_test_corpus/high_school.txt'], se.search('h   i there'))
+    assert_equals([], se.search('@&$* ___ &#*($#   $(&'))
+    assert_equals(['ryka_test_corpus/best_food.txt', 'ryka_test_corpus/high_school.txt'], se.search('in the the'))
+    assert_equals([], se.search('c0lor    '))
+    assert_equals(['ryka_test_corpus/favorite_color.txt'], se.search('   ()[]() c0l0r'))
+
+def test_blank_query(se):
+    """
+    Tests search method in SearchEngine class with blank parameter
+    """
+    assert_equals([], se.search(''))
+
+def main():
+    doc1 = Document('ryka_test_corpus/best_food.txt')
+    doc2 = Document('ryka_test_corpus/favorite_color.txt')
+    doc3 = Document('ryka_test_corpus/high_school.txt')
+    se = SearchEngine('ryka_test_corpus/')
+
+    #tests all the Document methods with ryka_test_corpus
+    test_term_frequency(doc1, doc2, doc3)
+    test_get_words(doc1, doc2, doc3)
+    test_get_path(doc1, doc2, doc3)
+
+    #tests every Search Engine method with ryka_test_corpus
+    test_calculate_idf(se)
+    test_single_item_query(se)
+    test_multiple_item_query(se)
+    test_blank_query(se)
+
+if __name__ == '__main__':
+    main()
---
+++ search_engine.py
@@ -0,0 +1,79 @@
+"""
+Ryka Chandra
+Intermediate Data Programming
+"""
+
+from document import Document
+from math import log
+import os
+import re
+
+
+def normalize_token(token):
+    token = token.lower()
+    # try eliminating everything except a-z
+    term = re.sub(r"[^a-z]+", "", token)
+    # if we removed everything, try keeping only numbers
+    if len(term) == 0:
+        # replace everything that is not 0-9
+        term = re.sub(r"\D+", "", token)
+    return term
+
+
+class SearchEngine:
+    """
+    This class represents a corpus of Document objects and
+    includes methods to compute the tf–idf statistic between
+    each document and a given query
+    """
+
+    def __init__(self, directory):
+        """
+        Takes a str path to a directory and constructs an inverted
+        index associating each term in the corpus to the list of
+        documents that contain the term
+        """
+        self.inverted_index = dict()
+        self.documents = []
+        for filename in os.listdir(directory):
+            path = os.path.join(directory, filename)
+            doc = Document(path)
+            self.documents.append(doc)
+            words = doc.get_words()
+            if len(words) != 0:
+                for word in words:
+                    if word in self.inverted_index:
+                        self.inverted_index[word].append(doc)
+                    else:
+                        self.inverted_index[word] = [doc]
+
+    def _calculate_idf(self, term):
+        """
+        Takes a str term and returns the inverse document frequency
+        of that term. If the term is not in the corpus, returns 0
+        """
+        if term not in self.inverted_index:
+            return 0
+        else:
+            return log(len(self.documents) / len(self.inverted_index[term]))
+
+    def search(self, query):
+        """
+        Takes a str query that contains one or more terms and returns a list
+        of document paths sorted in descending order by tf–idf statistic.
+        It normalizes the terms before processing and if there are no matching
+        documents, returns an empty list
+        """
+        docs = self.documents
+        tfidf_list = []
+        terms = query.split()
+        for doc in docs:
+            tfidf = 0
+            for term in terms:
+                term = normalize_token(term)
+                tfidf += doc.term_frequency(term) * self._calculate_idf(term)
+            if tfidf > 0:
+                tfidf_list.append((doc, tfidf))
+        tfidf_list.sort(key=lambda x: x[1], reverse=True)
+        path_list = [i[0].get_path() for i in tfidf_list]
+        return path_list
